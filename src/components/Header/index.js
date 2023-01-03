b1beb3a907f0d0c3126451d2a423d1fe01b72972
AlcenirSantos <alcenir.santos.dev@gmail.com>
2023-01-03 09:16:18 -0300

    Feat: Create Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import './styles.css';
+import { Link } from 'react-router-dom';
+export default function Header() {
+    return (
+        <header>
+            <Link className='logo' to='/'>Prime Flix</Link>
+            <Link className='favoritos' to='/favoritos'>Meus filmes</Link>
+        </header>
+    );
+}
